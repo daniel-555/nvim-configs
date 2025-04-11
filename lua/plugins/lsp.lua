7516d4eeae4e10d92e199bc52be0d9c245dd0ad2
Daniel Spiers <danjspiers@gmail.com>
2025-04-11 15:54:34 +0100

    changed formatting keybind from <F3> to <leader>cf

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -93,6 +93,7 @@ return {
                     local opts = { buffer = event.buf }
 
                     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                    vim.keymap.set({ 'n', 'v' }, '<leader>cf', '<cmd>lua vim.lsp.buf.format()<cr>', opts)
                     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
                     vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
                     vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
@@ -103,7 +104,6 @@ return {
                     vim.keymap.set('n', '[d', '<cmd>lua  vim.diagnostic.goto_next()<cr>', opts)
                     vim.keymap.set('n', ']d', '<cmd>lua  vim.diagnostic.goto_prev()<cr>', opts)
                     vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
                     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
                 end,
             })
