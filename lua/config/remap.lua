dc08f27718b5c98919394f9099d2b148400ea490
Daniel Spiers <danjspiers@gmail.com>
2025-04-15 21:25:54 +0100

    changed :nohl bind from <esc> to <leader>nh

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,5 +1,5 @@
 vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
-vim.keymap.set("n", "<esc>", ":nohl<CR>")
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
