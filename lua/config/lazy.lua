76d34ef721921e97be4de5945de5f40439093896
Daniel Spiers <danjspiers@gmail.com>
2025-05-08 23:44:24 +0100

    added onedark theme and general updates

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -72,7 +72,7 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { 'sonokai' } },
+    install = { colorscheme = { 'onedark' } },
     -- automatically check for plugin updates
     checker = { enabled = true },
 })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,22 @@
 return {
-    'sainnhe/sonokai',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.sonokai_style = 'shusia' 
-        vim.g.sonokai_enable_italic = true
-        vim.cmd.colorscheme('sonokai')
-    end
+    {
+        'sainnhe/sonokai',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.g.sonokai_style = 'shusia'
+            vim.g.sonokai_enable_italic = true
+        end
+    },
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            local onedark = require('onedark')
+            onedark.setup {
+                style = 'deep'
+            }
+            onedark.load()
+        end
+    }
 }
