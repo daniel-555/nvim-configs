0411709a067b097aaffd653818da907af0f75db1
Daniel Spiers <danjspiers@gmail.com>
2025-05-24 14:21:54 +0000

    updated telescope to search hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,15 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+        pickers = {
+            live_grep = {
+                glob_pattern = "!**/.git/*",
+                additional_args = { "--hidden" },
+            },
+            find_files = {
+                find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+            }
+        }
+    }
 }
