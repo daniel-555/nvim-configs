af0f27e2b03ca7bc4ae2a52851e7ccb29539d88a
Daniel Spiers <danjspiers@gmail.com>
2025-05-24 14:38:29 +0000

    changed move line keybinds to stop clashing with tmux

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>fd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv")
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv")
-vim.keymap.set("n", "<A-j>", ":m +1<cr>V=")
-vim.keymap.set("n", "<A-k>", ":m -2<cr>V=")
+vim.keymap.set("v", "<A-J>", ":m '>+1<cr>gv=gv")
+vim.keymap.set("v", "<A-K>", ":m '<-2<cr>gv=gv")
+vim.keymap.set("n", "<A-J>", ":m +1<cr>V=")
+vim.keymap.set("n", "<A-K>", ":m -2<cr>V=")
 
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
