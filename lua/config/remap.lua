140f8eb5a1142763baee441ac04e38178f843bd0
Daniel <daniel@riptidewave.com>
2025-09-08 11:47:15 +0100

    refactored lsp config to 2 files and now using blink.cmp

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -13,3 +13,31 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 
 vim.keymap.set({"n", "v"}, "<leader>y", "\"+y")
 vim.keymap.set({"n", "v"}, "<leader>p", "\"+p")
+
+vim.api.nvim_create_autocmd(
+    "LspAttach",
+    {
+        group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+        callback = function(ev)
+            vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
+
+            -- Buffer local mappings
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local opts = { buffer = ev.buf }
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            vim.keymap.set({ 'n', 'v' }, '<leader>fm', vim.lsp.buf.format, opts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+            vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
+            vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+            vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+            vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+        end
+    }
+)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,140 +1,22 @@
 return {
     {
-        'williamboman/mason.nvim',
-        lazy = false,
-        opts = {},
-    },
-
-    -- Autocompletion
-    {
-        'hrsh7th/nvim-cmp',
-        event = 'InsertEnter',
-        config = function()
-            local cmp = require('cmp')
-            local luasnip = require('luasnip')
-
-            cmp.setup({
-                sources = {
-                    { name = 'nvim_lsp' },
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<C-Space>'] = cmp.mapping.complete(),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-j>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-                    ['<C-k>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-                    ['<CR>'] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif luasnip.locally_jumpable(1) then
-                            luasnip.jump(1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-
-                }),
-                snippet = {
-                    expand = function(args)
-                        vim.snippet.expand(args.body)
-                    end,
-                },
-                formatting = {
-                    format = require('lspkind').cmp_format({
-                        mode = 'symbol_text', -- show only symbol annotations
-
-                        maxwidth = {
-                            -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-                            -- can also be a function to dynamically calculate max width such as
-                            -- menu = function() return math.floor(0.45 * vim.o.columns) end,
-                            menu = 50,            -- leading text (labelDetails)
-                            abbr = 50,            -- actual suggestion item
-                        },
-                        ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-                        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
-                    })
-                },
-                window = {
-                    completion = {
-                        border = 'rounded',
-                        winhighlight = 'Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None',
-                    },
-                    documentation = {
-                        border = 'rounded',
-                        winhighlight = 'Normal:Normal,FloatBorder:Normal,CursorLine:Visual,Search:None',
-                    },
-                },
-            })
-        end,
-        dependencies = { 'onsails/lspkind.nvim', 'L3MON4D3/LuaSnip' }
-    },
-
-    -- LSP
-    {
-        'neovim/nvim-lspconfig',
-        cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-        event = { 'BufReadPre', 'BufNewFile' },
+        "williamboman/mason.nvim",
         dependencies = {
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+            "williamboman/mason-lspconfig.nvim",
+            "neovim/nvim-lspconfig",
         },
-        init = function()
-            -- Reserve a space in the gutter
-            -- This will avoid an annoying layout shift in the screen
-            vim.opt.signcolumn = 'yes'
-        end,
         config = function()
-            local lsp_defaults = require('lspconfig').util.default_config
+            require("mason").setup()
 
-            -- Add cmp_nvim_lsp capabilities settings to lspconfig
-            -- This should be executed before you configure any language server
-            lsp_defaults.capabilities = vim.tbl_deep_extend(
-                'force',
-                lsp_defaults.capabilities,
-                require('cmp_nvim_lsp').default_capabilities()
-            )
-
-            -- LspAttach is where you enable features that only work
-            -- if there is a language server active in the file
-            vim.api.nvim_create_autocmd('LspAttach', {
-                desc = 'LSP actions',
-                callback = function(event)
-                    local opts = { buffer = event.buf }
-
-                    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                    vim.keymap.set({ 'n', 'v' }, '<leader>fm', vim.lsp.buf.format, opts)
-                    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
-                    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
-                    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-                    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-                    vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
-                    vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
-                    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                    vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
-                end,
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "ts_ls" }
             })
 
             vim.diagnostic.config {
                 severity_sort = true,
                 float = { border = 'rounded', source = 'if_many' },
-                underline = { severity = vim.diagnostic.severity.ERROR },
+                -- underline = { severity = vim.diagnostic.severity.ERROR },
+                underline = false,
                 signs = {
                     text = {
                         [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -143,7 +25,7 @@ return {
                         [vim.diagnostic.severity.HINT] = '󰌶 ',
                     },
                 },
-                virtual_text = {
+                virtual_lines = {
                     source = 'if_many',
                     spacing = 2,
                     format = function(diagnostic)
@@ -157,39 +39,27 @@ return {
                     end,
                 },
             }
+        end
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = { 'saghen/blink.cmp' },
 
-
-            require('mason-lspconfig').setup({
-                ensure_installed = {},
-                handlers = {
-                    -- this first function is the "default handler"
-                    -- it applies to every language server without a "custom handler"
-                    function(server_name)
-                        require('lspconfig')[server_name].setup({})
-                    end,
-
-                    ['lua_ls'] = function()
-                        require('lspconfig').lua_ls.setup {
-                            settings = {
-                                Lua = {
-                                    diagnostics = {
-                                        globals = { "vim" }
-                                    }
-                                }
-                            }
-                        }
-                    end,
-
-                    ['clangd'] = function()
-                        require('lspconfig').clangd.setup {
-                            cmd = {
-                                "clangd",
-                                "--fallback-style=webkit"
-                            }
-                        }
-                    end
-                }
-            })
+        -- example using `opts` for defining servers
+        opts = {
+            servers = {
+                lua_ls = {},
+                ts_ls = {},
+            }
+        },
+        config = function(_, opts)
+            local lspconfig = require('lspconfig')
+            for server, config in pairs(opts.servers) do
+                -- passing config.capabilities to blink.cmp merges with the capabilities in your
+                -- `opts[server].capabilities, if you've defined it
+                config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+                lspconfig[server].setup(config)
+            end
         end
     }
 }
