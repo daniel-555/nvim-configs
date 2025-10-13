d5129790e0a7fbc3a653055d5b45a156dac876b4
Daniel <daniel@riptidewave.com>
2025-10-13 10:46:48 +0100

    updated lsp confit to new framework

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -53,12 +53,12 @@ return {
             }
         },
         config = function(_, opts)
-            local lspconfig = require('lspconfig')
+            local lspconfig = vim.lsp.config
             for server, config in pairs(opts.servers) do
                 -- passing config.capabilities to blink.cmp merges with the capabilities in your
                 -- `opts[server].capabilities, if you've defined it
                 config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
+                lspconfig[server] = config;
             end
         end
     }
